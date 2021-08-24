3242b55d854b4f57136250690066acf1c5c53869
Krzysztof Kuta <atuk.krzysztof@gmail.com>
2021-08-24 21:25:09 +0200

    push to heroku

--- blomain/settings.py
+++ blomain/settings.py
@@ -121,9 +121,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATICFILES_DIRS = ['static']
 
 
 # Default primary key field type
