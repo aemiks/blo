aca214a6952689856950bc8de54280b79bb6a801
Krzysztof Kuta <atuk.krzysztof@gmail.com>
2021-08-24 22:09:55 +0200

    push to github

--- blomain/settings.py
+++ blomain/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['blo-dev.herokuapp.com', 'localhost:8000']
+ALLOWED_HOSTS = ['blo-dev.herokuapp.com', 'localhost:8000', '127.0.0.1']
 
 
 # Application definition
